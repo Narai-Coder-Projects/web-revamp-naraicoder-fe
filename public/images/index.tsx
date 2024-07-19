defa965b6a735af9dcc4b67cbc9c00061e47a41c
Diananaa <d1anasite04@gmail.com>
2024-07-19 16:52:28 +0700

    fix[#23]: fixing ui form

--- public/images/index.tsx
+++ public/images/index.tsx
@@ -1,15 +1,19 @@
 import ImgAboutHero from './image-about-naraicoder.svg'
 import ImgBatangGaringRight from './image-batang-garing-right.svg'
+import ImgBatangGaringLeft from './image-batang-garing-left.svg'
 import bgRegisterEventHero1 from './Ellipse 1.svg'
 import bgRegisterEventHero2 from './Ellipse 3.svg'
+import bgRegisterEventHero3 from './Ellipse 9.svg'
 import ImgSignIn from './sign-in-image.svg'
 import ImgSignUp from './sign-up-image.svg'
 
 export {
     ImgAboutHero,
     ImgBatangGaringRight,
+    ImgBatangGaringLeft,
     bgRegisterEventHero1,
     bgRegisterEventHero2,
+    bgRegisterEventHero3,
     ImgSignIn,
     ImgSignUp
 }
--- src/app/acara/page.tsx
+++ src/app/acara/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 import { Footer, Navbar } from "../components/Molecules"
 import Image from "next/image"
-import { bgRegisterEventHero1, bgRegisterEventHero2, ImgBatangGaringRight, ImgSignIn } from "../../../public/images"
+import { bgRegisterEventHero1, bgRegisterEventHero2, bgRegisterEventHero3, ImgBatangGaringLeft, ImgBatangGaringRight, ImgSignIn } from "../../../public/images"
 import { FormPendaftaran, SliderEvent } from "./components"
 
 const page = () => {
@@ -10,7 +10,7 @@ const page = () => {
         <>
             <Navbar />
             <div className=" flex flex-col items-center justify-center">
-                <div className="text-secondary bg-blue-50 py-[32px] md:py-0 md:bg-[#FDFDFE]  md:h-[50vh]  text-center flex-col w-full mt-[70px] md:mt-[70px]  sm:flex gap-2  sm:items-center">
+                <div className="text-secondary bg-blue-50 py-[32px] md:py-0 md:bg-[#FDFDFE] md:h-[50vh]  text-center flex-col w-full mt-[70px] md:mt-[70px]  sm:flex gap-2  sm:items-center">
                     <div className="z-10  flex flex-col items-center  md:h-[50vh]  justify-between ">
                         <div>
                             <p className="text-[18px] md:text-[26px] font-bold">In Collaboartion Naraicoder x Jagoan Hosting x Hactiv8 </p>
@@ -28,19 +28,29 @@ const page = () => {
                         <Image alt="#" src={bgRegisterEventHero1} width={243} height={128} />
                     </div>
                     <Image className="hidden sm:flex absolute w-[484px] h-[159px] left-1/2 top-30  z-0" alt="#" src={bgRegisterEventHero2} width={504} height={159} />
-
                 </div>
                 <section className="md:bg-secondary py-4 md:py-12 w-full text-white flex items-center justify-center">
                     <div className="lg:w-[1000px]">
                         <SliderEvent datas={datasa} />
                     </div>
                 </section>
-                <section id="daftar" className="my-10">
-                    <div className="lg:w-[1000px] h-[410px] flex justify-center">
-                        <FormPendaftaran />
-                    </div>
-                </section>
             </div>
+            <section id="daftar" >
+                <div className="relative  flex justify-center">
+                    <div className="hidden sm:flex absolute top-0 left-0  z-0 ">
+                        <Image alt="#" src={ImgBatangGaringLeft} width={210} height={300} />
+                    </div>
+                    <div>
+                        <div className="lg:w-[1000px] py-10 h-[450px] flex justify-center">
+                            <FormPendaftaran />
+                        </div>
+                        <div className="hidden sm:flex absolute bottom-0 right-0  z-0 ">
+                            <Image alt="#" src={bgRegisterEventHero3} width={343} height={168} />
+                        </div>
+                    </div>
+
+                </div>
+            </section>
             <Footer />
         </>
     )
